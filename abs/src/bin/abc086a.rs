9a181b35d7bc4b83c8d1f9c4acb186d91e4e574b
yukihir0 <yukihiro.cotori@gmail.com>
2020-10-04 11:38:23 +0900

    solve abc086a

--- abs/src/bin/abc086a.rs
+++ abs/src/bin/abc086a.rs
@@ -1,3 +1,14 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        a: u32,
+        b: u32,
+    }
+
+    if (a * b) % 2 == 0 {
+        println!("Even");
+    } else {
+        println!("Odd");
+    }
 }
