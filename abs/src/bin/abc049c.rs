86a8aee0a29d807775a3a39c3a057880f84b1faf
yukihir0 <yukihiro.cotori@gmail.com>
2020-10-04 14:44:25 +0900

    solve abc049c

--- abs/src/bin/abc049c.rs
+++ abs/src/bin/abc049c.rs
@@ -1,3 +1,11 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        s: String,
+    }
+
+    let patterns = ["eraser", "erase", "dreamer", "dream"];
+    let r = patterns.iter().fold(s, |t, x| t.replace(x, ""));
+    println!("{}", if r.is_empty() { "YES" } else { "NO " })
 }
