fef03b6e1be47b02e86f2fbc20703fa6d9565c15
yukihir0 <yukihiro.cotori@gmail.com>
2020-10-04 13:30:04 +0900

    solve abc085c

--- abs/src/bin/abc085c.rs
+++ abs/src/bin/abc085c.rs
@@ -1,3 +1,20 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: u32,
+        y: u32,
+    }
+
+    for i in (0..(n + 1)).rev() {
+        for j in (0..(n - i + 1)).rev() {
+            let k = n - i - j;
+            if 10000 * i + 5000 * j + 1000 * k == y {
+                println!("{} {} {}", i, j, k);
+                return;
+            }
+        }
+    }
+
+    println!("-1 -1 -1");
 }
