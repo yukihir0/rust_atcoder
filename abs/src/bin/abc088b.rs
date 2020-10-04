7cce19aa98afa4234bb71c3732d1803568e4694b
yukihir0 <yukihiro.cotori@gmail.com>
2020-10-04 12:58:54 +0900

    solve abc088b

--- abs/src/bin/abc088b.rs
+++ abs/src/bin/abc088b.rs
@@ -1,3 +1,21 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: u32,
+        mut a: [u32; n],
+    }
+
+    a.sort_by(|x, y| x.cmp(y).reverse());
+
+    let mut alice = 0;
+    let mut bob = 0;
+    for (i, &x) in a.iter().enumerate() {
+        if i % 2 == 0 {
+            alice += x;
+        } else {
+            bob += x;
+        }
+    }
+    println!("{}", alice - bob);
 }
