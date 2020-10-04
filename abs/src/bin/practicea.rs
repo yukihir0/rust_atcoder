da99b82bbc2ebc05bddd702ee713672feebf5cea
yukihir0 <yukihiro.cotori@gmail.com>
2020-10-04 11:30:07 +0900

    solve practicea

--- abs/src/bin/practicea.rs
+++ abs/src/bin/practicea.rs
@@ -1,3 +1,12 @@
+use proconio::{input};
+
 fn main() {
-    unimplemented!();
+    input!{
+        a: usize,
+        b: usize,
+        c: usize,
+        s: String,
+    }
+
+    println!("{} {}", a+b+c, s);
 }
