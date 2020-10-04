6ab78141fcd5ca3e19928bbdc15e5bcfe776bfe9
yukihir0 <yukihiro.cotori@gmail.com>
2020-10-04 12:47:26 +0900

    solve abc083b

--- abs/src/bin/abc083b.rs
+++ abs/src/bin/abc083b.rs
@@ -1,3 +1,21 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: u32,
+        a: u32,
+        b: u32,
+    }
+
+    let ans = (1..n + 1)
+        .filter(|x| {
+            let sum = x
+                .to_string()
+                .chars()
+                .map(|c| (c as u8 - b'0') as u32)
+                .sum::<u32>();
+            a <= sum && sum <= b
+        })
+        .sum::<u32>();
+    println!("{}", ans);
 }
