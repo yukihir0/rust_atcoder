bfcee1f2b15986ee0125ebd635d5eb111c960733
yukihir0 <yukihiro.cotori@gmail.com>
2020-10-04 12:24:13 +0900

    solve abc081b

--- abs/src/bin/abc081b.rs
+++ abs/src/bin/abc081b.rs
@@ -1,3 +1,11 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: u32,
+        a: [u32; n],
+    };
+
+    let ans = a.iter().map(|&v| v.trailing_zeros()).min().unwrap();
+    println!("{}", ans);
 }
