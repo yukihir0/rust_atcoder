545c02e2995e431b5d0c4fe48da0766dc7215bed
yukihir0 <yukihiro.cotori@gmail.com>
2020-10-04 12:37:50 +0900

    solve abc087b

--- abs/src/bin/abc087b.rs
+++ abs/src/bin/abc087b.rs
@@ -1,3 +1,23 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        a: u32,
+        b: u32,
+        c: u32,
+        x: u32,
+    }
+
+    let mut ans = 0;
+    for i in 0..(a + 1) {
+        for j in 0..(b + 1) {
+            for k in 0..(c + 1) {
+                if 500 * i + 100 * j + 50 * k == x {
+                    ans += 1;
+                }
+            }
+        }
+    }
+
+    println!("{}", ans);
 }
