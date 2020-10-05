ffb3a5b915467aa18c8accd31ae8dac3fcb32486
yukihir0 <yukihiro.cotori@gmail.com>
2020-10-05 21:45:36 +0900

    solve abc086c

--- abs/src/bin/abc086c.rs
+++ abs/src/bin/abc086c.rs
@@ -1,3 +1,18 @@
+use proconio::input;
+
 fn main() {
-    unimplemented!();
+    input! {
+        n: u32,
+        mut v: [(i32, i32, i32); n],
+    }
+
+    v.insert(0, (0, 0, 0));
+    let yes = v[..].windows(2).all(|w| {
+        let (t, x, y) = w[0];
+        let (nt, nx, ny) = w[1];
+        let time = nt - t;
+        let dist = (nx - x).abs() + (ny - y).abs();
+        dist <= time && time % 2 == dist % 2
+    });
+    println!("{}", if yes { "Yes" } else { "No" });
 }
